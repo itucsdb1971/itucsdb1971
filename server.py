d47f44aae22e8e40fcd73e95c83062b41da7bd2f
Emre Horsanalı <emrehorsanali@outlook.com>
2019-10-22 11:15:03 +0300

    Small fix related to scope.

--- server.py
+++ server.py
@@ -1,17 +1,10 @@
 from flask import Flask
 import views
 
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
-
-    app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/movies", view_func=views.movies_page)
-
-    return app
-
+app.add_url_rule("/", view_func=views.home_page)
+app.add_url_rule("/movies", view_func=views.movies_page)
 
 if __name__ == "__main__":
-    app = create_app()
-
     app.run()
